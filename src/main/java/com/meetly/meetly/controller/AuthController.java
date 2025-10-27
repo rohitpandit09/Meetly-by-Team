30ed9680e94a69191db25c5cc0f0bcb7ce985ca9
Rohit Pandit <rpandit0907@gmail.com>
2025-10-27 22:56:43 +0530

    Deploying the website by fixing a bug

--- src/main/java/com/meetly/meetly/controller/AuthController.java
+++ src/main/java/com/meetly/meetly/controller/AuthController.java
@@ -11,6 +11,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/auth")
 @CrossOrigin(origins = "*") // Allow frontend from any origin
+
 public class AuthController {
 
     @Autowired
